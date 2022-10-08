8cc5475a013433a72aca3930975eea14190175f3
vokur13 <vokur13@gmail.com>
2022-10-08 20:30:57 +0300

    AddMore Button resolved

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'css/styles.css';
-import { Box } from 'components/Box';
-import { Button } from 'components/Button';
 import { Searchbar } from 'components/Searchbar';
 import { ImageGalleryHub } from 'components/ImageGalleryHub';
 
 export class App extends Component {
   state = {
     query: '',
-    page: 1,
   };
 
   handleFormSubmit = ({ query }) => {
@@ -21,22 +18,12 @@ export class App extends Component {
     this.setState({ query: q });
   };
 
-  handleMoreImage = () => {
-    console.log('Handle More Image');
-    this.setState(prevState => ({ page: prevState.page + 1 }));
-  };
-
   render() {
-    const { query, page } = this.state;
+    const { query } = this.state;
     return (
       <>
         <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGalleryHub query={query} page={page} />
-        <Box display="flex" justifyContent="center">
-          <Button type="button" onClick={this.handleMoreImage}>
-            Load more
-          </Button>
-        </Box>
+        <ImageGalleryHub query={query} />
         <ToastContainer position="top-left" autoClose={5000} />
       </>
     );
--- src/components/ImageGalleryHub/ImageGalleryHub.jsx
+++ src/components/ImageGalleryHub/ImageGalleryHub.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
 import { toast } from 'react-toastify';
 import * as API from 'services/api';
+import { Box } from 'components/Box';
+import { Button } from 'components/Button';
 import { ImageGalleryError } from 'components/ImageGalleryError';
 import { ImageGallery } from 'components/ImageGallery';
 import { ImageGalleryPending } from 'components/ImageGalleryPending';
@@ -13,15 +15,22 @@ const Status = {
 };
 
 export class ImageGalleryHub extends Component {
+  static defaultProps = {
+    step: 1,
+    initialValue: 1,
+  };
+
   state = {
     gallery: [],
+    page: this.props.initialValue,
     error: false,
     length: null,
     status: Status.IDLE,
   };
 
   async componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.props;
+    const { query } = this.props;
+    const { page } = this.state;
     if (prevProps.query !== query) {
       try {
         this.setState({
@@ -45,7 +54,7 @@ export class ImageGalleryHub extends Component {
         console.log(error);
       }
     }
-    if (prevProps.page !== page) {
+    if (prevState.page !== page) {
       try {
         this.setState({
           status: Status.PENDING,
@@ -64,6 +73,10 @@ export class ImageGalleryHub extends Component {
     }
   }
 
+  handleMoreImage = async () => {
+    this.setState(prevState => ({ page: prevState.page + this.props.step }));
+  };
+
   render() {
     const { query } = this.props;
     const { gallery, error, status } = this.state;
@@ -81,7 +94,16 @@ export class ImageGalleryHub extends Component {
     }
 
     if (status === 'resolved') {
-      return <ImageGallery data={gallery} />;
+      return (
+        <>
+          <ImageGallery data={gallery} />;
+          <Box display="flex" justifyContent="center">
+            <Button type="button" onClick={this.handleMoreImage}>
+              Load more
+            </Button>
+          </Box>
+        </>
+      );
     }
   }
 }
