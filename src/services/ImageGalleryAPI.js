fb2ea6d3e36c4d0792cd2097297d3f1f8d56edba
vokur13 <vokur13@gmail.com>
2022-10-08 09:58:39 +0300

    project init

--- src/services/ImageGalleryAPI.js
+++ src/services/ImageGalleryAPI.js
@@ -4,9 +4,11 @@ axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const API_KEY = '29248542-cea93977a5234fa0e2d1b3dfd';
 
-export const fetchGallery = async (query, page) => {
-  const response = await axios.get(
+export const fetchGallery = (query, page) => {
+  const response = axios.get(
     `/?q=${query}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
   );
   return response.data.hits;
 };
+
+const url = `https://pixabay.com/api/?q=london&page=1&key=29248542-cea93977a5234fa0e2d1b3dfd&image_type=photo&orientation=horizontal&per_page=12`;
